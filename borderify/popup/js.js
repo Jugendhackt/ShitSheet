c6025172bcf8ac1da6424393bea88d56c3a442ab
Frieder21 <Janick@welttraeumer.de>
2019-10-06 01:34:36 +0200

    Add-on fast fertig für firefox

--- borderify/popup/js.js
+++ borderify/popup/js.js
@@ -1,12 +1,11 @@
+//variable
 
+
+//funktionen
 function getCurrentURL(){
   function onGot(tabsInfo) {
-    console.log(tabsInfo);
     var tab = tabsInfo[0]
-    console.log(tab.url);
-
-
-
+    save(tabsInfo[0])
   }
 
   function onError(error) {
@@ -20,4 +19,67 @@ function getCurrentURL(){
     chrome.tabs.query({ active: true, currentWindow: true },onGot);
   }
 }
+
+function load(){
+  var content = localStorage.links ;
+  console.log(content)
+  var list = JSON.parse(content);
+  return list
+  }
+function show_list(){
+  var X = document.getElementById("tabelle")
+  X.innerHTML = ""
+  var Z = load()
+  for(i=0;i<Z.length;i++){
+  var title = Z[i].title
+  if (title.length > 50){
+    title = title.substring(0,49)+"..."
+  }
+  X.innerHTML  += "<tr><td>"+title+"</td>"+/*<td><button id='"+i+"'></button></td>*/"</tr>"
+  }
+/*
+  for(i=0;i<Z.length;i++){
+  document.getElementById(i).onclick = function (e){
+    e.target.parentNode.parentNode.RemoveChild(e.target.parentNode);
+  };
+  }
+  */
+}
+function save(tab){
+  var Linkliste = load()
+    Linkliste.push({url:tab.url, title:tab.title})
+    localStorage.links = JSON.stringify(Linkliste);
+    show_list()
+}
+function clear(){
+  var Linkliste = [];
+  localStorage.links = JSON.stringify(Linkliste);
+  show_list()
+}
+function loeschen(i){
+alert("error"+i)
+}
+function download(){
+JSON.parse(content)
+
+}
+
+function download() {
+  var element = document.createElement('a');
+  var content = localStorage.links
+  element.setAttribute('href', 'data:LOL;charset=utf-8,' + encodeURIComponent(content));
+  element.setAttribute('download', "shitSheet.json");
+
+  element.style.display = 'none';
+  document.body.appendChild(element);
+
+  element.click();
+
+  document.body.removeChild(element);
+}
+//Autostart
+
 document.getElementById('add_entry').onclick = getCurrentURL;
+document.getElementById('clear').onclick = clear;
+document.getElementById('download').onclick = download;
+show_list()
