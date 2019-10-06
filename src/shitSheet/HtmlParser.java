8ebe9be2976e211f219ce801388db0342ea49792
Ulf_vom_Mond <noahrullmann@gmail.com>
2019-10-06 02:41:14 +0200

    now all stack overflow threads can be parsed. i think

--- src/shitSheet/HtmlParser.java
+++ src/shitSheet/HtmlParser.java
@@ -1,8 +1,10 @@
 package shitSheet;
 
 import java.io.BufferedReader;
+import java.io.Console;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.Reader;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.net.URLConnection;
@@ -12,12 +14,15 @@ public class HtmlParser {
 	public static void main(String[] args) {
 		
 		URL url;
-		String html = "dnjs";
+		String html = "";
 		
 		try {
             // get URL content
-
-            String a="https://stackoverflow.com/questions/5956627/python-universal-import";
+//			System.out.println("https://stackoverflow.com/questions/5956627/python-universal-import\n");
+			BufferedReader br1 = new BufferedReader(new InputStreamReader(System.in));
+		    String a = br1.readLine();
+//            String a="https://stackoverflow.com/questions/5956627/python-universal-import";
+            
             url = new URL(a);
             URLConnection conn = url.openConnection();
 
@@ -27,13 +32,13 @@ public class HtmlParser {
 
             String inputLine;
             while ((inputLine = br.readLine()) != null) {
-                    System.out.println(inputLine);
+                    //System.out.println(inputLine);
                     //html = html.concat(inputLine);
                     html = html + "\n" +  inputLine;
             }
             br.close();
 
-            System.out.println("Done");
+            //System.out.println("Done");
 
         } catch (MalformedURLException e) {
             e.printStackTrace();
@@ -43,15 +48,39 @@ public class HtmlParser {
 		String searchTerm = "class=\"question-hyperlink\"";
 		int a = html.indexOf(">", html.indexOf(searchTerm));
 		String title =  html.substring(a + 1, html.indexOf("<", a));
-		int startIndex = html.indexOf("<div id=\"answers\">");
+//		int startIndex = html.indexOf("<div id=\"answers\">");
+		int startIndex = html.indexOf("class=\"answer accepted-answer\"");
+		int endIndex = startIndex;
 		String code = "";
-		while (code.length() < 20) {
-			startIndex = html.indexOf("<code>", startIndex);
-			code = html.substring(startIndex + 6, html.indexOf("</code>", startIndex + 1));
-			startIndex++;
+		int divLayer = 0;
+//		System.out.println(endIndex);
+		while (divLayer != -1) {
+			endIndex = html.indexOf("div", endIndex + 1);
+			divLayer += html.charAt(endIndex - 1) == "<".charAt(0) ? 1 : -1;
+			//System.out.println(divLayer);
 		}
-		System.out.println("titel: " + title);
-		System.out.println(code);
+//		System.out.println(endIndex);
+		html = "<" + html.substring(startIndex, endIndex - 2);
+		startIndex = html.indexOf("div class=\"post-text\" itemprop=\"text\">");
+		endIndex = startIndex;
+		code = "";
+		divLayer = 0;
+//		System.out.println(endIndex);
+		while (divLayer != -1) {
+			endIndex = html.indexOf("div", endIndex + 1);
+			divLayer += html.charAt(endIndex - 1) == "<".charAt(0) ? 1 : -1;
+			//System.out.println(divLayer);
+		}
+		html = "<" + html.substring(startIndex, endIndex - 2);
+//		System.out.println(endIndex);
+//		while (code.length() < 20) {
+//			startIndex = html.indexOf("<code>", startIndex);
+//			code = html.substring(startIndex + 6, html.indexOf("</code>", startIndex + 1));
+//			startIndex++;
+//		}
+//		System.out.println("titel: " + title);
+		
+		System.out.println(html);
 	}
 
 }
