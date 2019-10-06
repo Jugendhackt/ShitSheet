e228d48320591fd467750d801e6197074258f05e
Ulf_vom_Mond <noahrullmann@gmail.com>
2019-10-06 09:35:46 +0200

    some cleanup, added SheetShitter class

--- src/shitSheet/HtmlParser.java
+++ src/shitSheet/HtmlParser.java
@@ -11,6 +11,57 @@ import java.net.URLConnection;
 
 public class HtmlParser {
 	
+	static String getDivContent(String html, String searchTerm) {
+		int startIndex = html.indexOf(searchTerm);
+		int endIndex = startIndex;
+		String code = "";
+		int divLayer = 0;
+//		System.out.println(endIndex);
+		while (divLayer != -1) {
+			endIndex = html.indexOf("div", endIndex + 1);
+			divLayer += html.charAt(endIndex - 1) == "<".charAt(0) ? 1 : -1;
+			//System.out.println(divLayer);
+		}
+//		System.out.println(endIndex);
+		html = "<" + html.substring(startIndex, endIndex - 2);
+		return html;
+	}
+	
+	String parse(URL url) {
+		
+		String html = "";
+		
+		try {
+            // get URL content
+//			System.out.println("https://stackoverflow.com/questions/5956627/python-universal-import\n");
+			BufferedReader br1 = new BufferedReader(new InputStreamReader(System.in));
+		    String a = br1.readLine();
+//            String a="https://stackoverflow.com/questions/5956627/python-universal-import";
+            
+            url = new URL(a);
+            URLConnection conn = url.openConnection();
+
+            // open the stream and put it into BufferedReader
+            BufferedReader br = new BufferedReader(
+                               new InputStreamReader(conn.getInputStream()));
+
+            String inputLine;
+            while ((inputLine = br.readLine()) != null) {
+                    html = html + "\n" +  inputLine;
+            }
+            br.close();
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+		String searchTerm = "class=\"question-hyperlink\"";
+		int a = html.indexOf(">", html.indexOf(searchTerm));
+		String title =  html.substring(a + 1, html.indexOf("<", a));
+		html = getDivContent(getDivContent(html, "class=\"answer accepted-answer\""), "div class=\"post-text\" itemprop=\"text\">");
+		return title + "<|>" + html;
+	}
+	
 	public static void main(String[] args) {
 		
 		URL url;
@@ -32,14 +83,9 @@ public class HtmlParser {
 
             String inputLine;
             while ((inputLine = br.readLine()) != null) {
-                    //System.out.println(inputLine);
-                    //html = html.concat(inputLine);
                     html = html + "\n" +  inputLine;
             }
             br.close();
-
-            //System.out.println("Done");
-
         } catch (MalformedURLException e) {
             e.printStackTrace();
         } catch (IOException e) {
@@ -48,39 +94,9 @@ public class HtmlParser {
 		String searchTerm = "class=\"question-hyperlink\"";
 		int a = html.indexOf(">", html.indexOf(searchTerm));
 		String title =  html.substring(a + 1, html.indexOf("<", a));
-//		int startIndex = html.indexOf("<div id=\"answers\">");
-		int startIndex = html.indexOf("class=\"answer accepted-answer\"");
-		int endIndex = startIndex;
-		String code = "";
-		int divLayer = 0;
-//		System.out.println(endIndex);
-		while (divLayer != -1) {
-			endIndex = html.indexOf("div", endIndex + 1);
-			divLayer += html.charAt(endIndex - 1) == "<".charAt(0) ? 1 : -1;
-			//System.out.println(divLayer);
-		}
-//		System.out.println(endIndex);
-		html = "<" + html.substring(startIndex, endIndex - 2);
-		startIndex = html.indexOf("div class=\"post-text\" itemprop=\"text\">");
-		endIndex = startIndex;
-		code = "";
-		divLayer = 0;
-//		System.out.println(endIndex);
-		while (divLayer != -1) {
-			endIndex = html.indexOf("div", endIndex + 1);
-			divLayer += html.charAt(endIndex - 1) == "<".charAt(0) ? 1 : -1;
-			//System.out.println(divLayer);
-		}
-		html = "<" + html.substring(startIndex, endIndex - 2);
-//		System.out.println(endIndex);
-//		while (code.length() < 20) {
-//			startIndex = html.indexOf("<code>", startIndex);
-//			code = html.substring(startIndex + 6, html.indexOf("</code>", startIndex + 1));
-//			startIndex++;
-//		}
 //		System.out.println("titel: " + title);
 		
-		System.out.println(html);
+		System.out.println(getDivContent(getDivContent(html, "class=\"answer accepted-answer\""), "div class=\"post-text\" itemprop=\"text\">"));
 	}
 
 }
---
+++ src/shitSheet/SheetShitter.java
@@ -0,0 +1,9 @@
+package shitSheet;
+
+public class SheetShitter {
+
+	public static void main(String[] args) {
+
+	}
+
+}
